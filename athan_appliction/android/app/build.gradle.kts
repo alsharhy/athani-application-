35eda29cd4b0042695c533199eeeb80d8dfcb285
alsharhy <dhayfalsharhi@gmail.com>
2026-06-13 05:09:25 +0300

    feat: initialize Android application module with MainActivity, build configuration, and manifest settings

--- athan_appliction/android/app/build.gradle.kts
+++ athan_appliction/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.athan_appliction"
+    namespace = "com.example.tast"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.athan_appliction"
+        applicationId = "com.example.tast"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
---
+++ athan_appliction/android/app/src/main/kotlin/com/example/tast/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.tast
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
